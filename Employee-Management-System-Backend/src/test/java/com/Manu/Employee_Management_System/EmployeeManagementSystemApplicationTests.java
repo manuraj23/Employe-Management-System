d60560f9d3065b6ab10a3090c479f0df6e5a4931
manuraj23 <manuraj082004@gmail.com>
2026-01-19 18:47:51 +0530

    Basic Files added

---
+++ Employee-Management-System-Backend/src/test/java/com/Manu/Employee_Management_System/EmployeeManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Manu.Employee_Management_System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
